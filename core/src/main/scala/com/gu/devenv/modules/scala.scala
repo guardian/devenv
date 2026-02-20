32862ba61b5908afb4b9858a86b06f51209daf18
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-02-20 15:20:09 +0000

    Add Ivy and Coursier caches

--- core/src/main/scala/com/gu/devenv/modules/scala.scala
+++ core/src/main/scala/com/gu/devenv/modules/scala.scala
@@ -1,13 +1,14 @@
 package com.gu.devenv.modules
 
-import com.gu.devenv.Plugins
 import com.gu.devenv.modules.Modules.{Module, ModuleContribution}
+import com.gu.devenv.{Command, Mount, Plugins}
 
 /** Provides IDE plugin support for Scala development.
   *
   * This module adds the Scala language plugin for both VS Code and IntelliJ IDEA.
   */
-private[modules] val scalaLang = Module(
+private[modules] val scalaLang = getScalaLang();
+private[modules] def getScalaLang(volumeName: String = "docker-sbt-data-volume") = Module(
   name = "scala",
   summary = "Add IDE plugins for Scala development",
   enabledByDefault = false,
@@ -15,6 +16,23 @@ private[modules] val scalaLang = Module(
     plugins = Plugins(
       intellij = List("org.intellij.scala"),
       vscode = List("scala-lang.scala")
+    ),
+    postCreateCommands = List(
+      Command(
+        cmd = """bash -c 'set -e && """ +
+          // ensure correct ownership of the shared ivy data volume
+          "sudo chown -R vscode:vscode /home/vscode/.sbt " +
+          """'""",
+        workingDirectory = "."
+      )
+    ),
+
+    mounts = List(
+      Mount.ExplicitMount(
+        source = volumeName,
+        target = "/home/vscode/.sbt",
+        `type` = "volume"
+      )
     )
   )
 )
