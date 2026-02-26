fd26116500249e3e41cdc9a6593acfc5227e0388
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-02-26 12:41:31 +0000

    Fix mount location and ensure it is identical in both places

--- core/src/main/scala/com/gu/devenv/modules/scala.scala
+++ core/src/main/scala/com/gu/devenv/modules/scala.scala
@@ -7,43 +7,47 @@ import com.gu.devenv.{Command, Mount, Plugins}
   *
   * This module adds the Scala language plugin for both VS Code and IntelliJ IDEA.
   */
-private[modules] def scalaLang(mountKey: String) = Module(
-  name = "scala",
-  summary = "Add IDE plugins for Scala development",
-  enabledByDefault = false,
-  contribution = ModuleContribution(
-    plugins = Plugins(
-      intellij = List("org.intellij.scala"),
-      vscode = List("scala-lang.scala")
-    ),
-    postCreateCommands = List(
-      Command(
-        cmd = """bash -c '
-            |set -e &&
-            |echo -e "\033[1;34m[setup] Setting up .ivy2 ...\033[0m" &&
-            |sudo chown -R vscode:vscode /home/vscode/.ivy2 &&
-            |
-            |echo -e "\033[1;34m[setup] Setting up coursier ...\033[0m" &&
-            |sudo chown -R vscode:vscode /.jbdevcontainer/coursier
-            |'
-            |""".stripMargin.split('\n').mkString(" "),
-        workingDirectory = "."
-      )
-    ),
-    /* All mounts bring security trade-offs as the volume is shared between all containers using this module.  */
-    mounts = List(
-      /* This mount persists the coursier cache across container recreations. */
-      Mount.ExplicitMount(
-        source = s"$mountKey-coursier-data-volume",
-        target = "/.jbdevcontainer/coursier",
-        `type` = "volume"
+private[modules] def scalaLang(mountKey: String) = {
+  val coursierCacheLocation = "/home/vscode/.cache/coursier/v1"
+  val ivy2CacheLocation     = "/home/vscode/.ivy2"
+  Module(
+    name = "scala",
+    summary = "Add IDE plugins for Scala development",
+    enabledByDefault = false,
+    contribution = ModuleContribution(
+      plugins = Plugins(
+        intellij = List("org.intellij.scala"),
+        vscode = List("scala-lang.scala")
       ),
-      /* This mount persists the ivy2 cache across container recreations. */
-      Mount.ExplicitMount(
-        source = s"$mountKey-ivy-data-volume",
-        target = "/home/vscode/.ivy2",
-        `type` = "volume"
+      postCreateCommands = List(
+        Command(
+          cmd = s"""bash -c '
+            |set -e &&
+            |echo -e "\\033[1;34m[setup] Setting up .ivy2 ...\\033[0m" &&
+            |sudo chown -R vscode:vscode $ivy2CacheLocation &&
+            |
+            |echo -e "\\033[1;34m[setup] Setting up coursier ...\\033[0m" &&
+            |sudo chown -R vscode:vscode $coursierCacheLocation
+            |
+            |'""".stripMargin.split('\n').mkString(" "),
+          workingDirectory = "."
+        )
+      ),
+      /* All mounts bring security trade-offs as the volume is shared between all containers using this module.  */
+      mounts = List(
+        /* This mount persists the coursier cache across container recreations. */
+        Mount.ExplicitMount(
+          source = s"$mountKey-coursier-data-volume",
+          target = coursierCacheLocation,
+          `type` = "volume"
+        ),
+        /* This mount persists the ivy2 cache across container recreations. */
+        Mount.ExplicitMount(
+          source = s"$mountKey-ivy-data-volume",
+          target = ivy2CacheLocation,
+          `type` = "volume"
+        )
       )
     )
   )
-)
+}
