364869096d67cdd8a1bf9b86856e8ec31aeae42f
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-03-19 16:19:56 +0000

    Do not fix devcontainer user name

--- core/src/main/resources/com/gu/devenv/modules/scalaOnCreateCommand.sh
+++ core/src/main/resources/com/gu/devenv/modules/scalaOnCreateCommand.sh
@@ -6,18 +6,35 @@ ok()    { printf "\033[1;32m[...] %s\033[0m\n" "$*"; }  # green
 warn()  { printf "\033[1;33m[...] %s\033[0m\n" "$*"; }  # gold
 log()   { printf "\033[1;36m[...] %s\033[0m\n" "$*"; }  # cyan
 
-if [[ -z $IVY_DATA_DIR_ROOT ]]; then
-    warn "IVY_DATA_DIR_ROOT not set"
+if [[ -z $IVY_DATA_DIR ]]; then
+    warn "IVY_DATA_DIR not set"
     exit 1
 fi
 
-if [[ -z $COURSIER_DATA_DIR_ROOT ]]; then
-    warn "COURSIER_DATA_DIR_ROOT not set"
-    exit 1
-fi
+IVY_CACHE_DIR_ROOT="/home/$(whoami)/.ivy2"
+IVY_CACHE_DIR_LINK="$IVY_CACHE_DIR_ROOT/cache"
+
+log "Link the shared ivy data volume at the correct point."
+mkdir -p "$IVY_CACHE_DIR_LINK"
+rmdir "$IVY_CACHE_DIR_LINK"
+ln -sf "$IVY_DATA_DIR" "$IVY_CACHE_DIR_LINK"
+sudo chown -R vscode:vscode "$IVY_CACHE_DIR_ROOT"
 
 log "Ensuring correct ownership of the shared ivy data volume."
-sudo chown -R vscode:vscode "$IVY_DATA_DIR_ROOT"
+sudo chown -R vscode:vscode "$IVY_CACHE_DIR_ROOT"
+
+if [[ -z $COURSIER_DATA_DIR ]]; then
+    warn "COURSIER_DATA_DIR not set"
+    exit 1
+fi
+
+COURSIER_CACHE_DIR_ROOT="/home/$(whoami)/.cache"
+COURSIER_CACHE_DIR_LINK="$COURSIER_CACHE_DIR_ROOT/coursier/v1"
+
+log "Link the shared coursier data volume at the correct point."
+mkdir -p "$COURSIER_CACHE_DIR_LINK"
+rmdir "$COURSIER_CACHE_DIR_LINK"
+ln -sf "$COURSIER_DATA_DIR" "$COURSIER_CACHE_DIR_LINK"
 
 log "Ensuring correct ownership of the shared coursier data volume."
-sudo chown -R vscode:vscode "$COURSIER_DATA_DIR_ROOT"
+sudo chown -R vscode:vscode "$COURSIER_CACHE_DIR_ROOT"
