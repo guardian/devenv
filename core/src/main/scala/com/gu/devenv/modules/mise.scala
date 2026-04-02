0a7d353454ced7f4a9f3f5f1f9a802421644df34
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-04-02 08:57:39 +0100

    Debug release

--- core/src/main/scala/com/gu/devenv/modules/mise.scala
+++ core/src/main/scala/com/gu/devenv/modules/mise.scala
@@ -1,7 +1,7 @@
 package com.gu.devenv.modules
 
 import com.gu.devenv.modules.Modules.{Module, ModuleContribution}
-import com.gu.devenv.{Command, Env, Mount, Plugins}
+import com.gu.devenv.{Command, Plugins}
 
 import scala.util.Try
 
@@ -16,43 +16,16 @@ import scala.util.Try
   * the complexity in a module. This allows teams to have a consistent development environment
   * without needing to manually install and configure `mise` each time.
   */
-private[modules] def mise(mountKey: String): Try[Module] =
+private[modules] def mise: Try[Module] =
   for {
-    encodedOnCreateScript   <- Command.fromResourceScript("miseOnCreateCommand.sh")
     encodedPostCreateScript <- Command.fromResourceScript("misePostCreateCommand.sh")
   } yield {
-    // Sets the DEVENV_MISE_CACHE_MOUNT_DIR to a shared volume (defined below) to cache downloaded tools and versions
-    val DEVENV_MISE_CACHE_MOUNT_DIR = "/mnt/mise-cache"
-    // This is not a DEVENV var, it's a MISE var, so no DEVENV_ namespacing.
-    val MISE_INSTALL_PATH = s"$DEVENV_MISE_CACHE_MOUNT_DIR/mise"
     Module(
       name = "mise",
       summary = "Install and configure mise for dev tools management (https://mise.jdx.dev/)",
       enabledByDefault = true,
       contribution = ModuleContribution(
-        onCreateCommands = List(encodedOnCreateScript),
         postCreateCommands = List(encodedPostCreateScript),
-        // Adds mise shims to the PATH so that installed tools are available in the remote environment
-        remoteEnv = List(
-          Env("PATH", s"$${containerEnv:PATH}:~/.local/share/mise/shims/")
-        ),
-        containerEnv = List(
-          Env("DEVENV_MISE_CACHE_MOUNT_DIR", DEVENV_MISE_CACHE_MOUNT_DIR),
-          Env("MISE_INSTALL_PATH", MISE_INSTALL_PATH)
-        ),
-        /* This mount persists the mise cache and installed tool versions across container recreations.
-         *
-         * This brings security trade-offs as the volume is shared between all containers using this module.
-         * However, without this shared volume, mise would need to re-download and re-install tools
-         * every time the container is recreated, which would be a significant usability issue.
-         */
-        mounts = List(
-          Mount.ExplicitMount(
-            source = s"$mountKey-mise-cache-volume",
-            target = DEVENV_MISE_CACHE_MOUNT_DIR,
-            `type` = "volume"
-          )
-        ),
         // provide IDE support for mise
         plugins = Plugins(
           intellij = List("com.github.l34130.mise"),
