ba2265416dcb9ebf4eb4bb583e953754f62434bb
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-04-07 08:45:47 +0100

    Convert to apt install

--- core/src/main/resources/com/gu/devenv/modules/misePostCreateCommand.sh
+++ core/src/main/resources/com/gu/devenv/modules/misePostCreateCommand.sh
@@ -8,18 +8,27 @@ ok()    { printf "\033[1;32m[...] %s\033[0m\n" "$*"; }  # green
 warn()  { printf "\033[1;33m[...] %s\033[0m\n" "$*"; }  # gold
 log()   { printf "\033[1;36m[...] %s\033[0m\n" "$*"; }  # cyan
 
-if which mise && mise --version; then
-  log "mise is already present at $(which mise)."
+if sudo dpkg -l | grep -w mise | grep '^ii'; then
+  log "Updating mise..."
+
+  sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/mise.list
+  sudo apt-get install --only-upgrade mise
 else
   log "Installing mise..."
-  curl -fsSL https://mise.run/bash | sh
+
+  sudo install -db 755 /etc/apt/keyrings
+  curl -Ls https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc
+
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" \
+    | sudo tee /etc/apt/sources.list.d/mise.list
+
+  sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/mise.list
+  sudo apt-get install -y mise
+
 fi
 
 log "Checking mise is working correctly and on the path."
 mise --version
-
-log "Updating mise if needed."
-mise self-update --yes || warn "Skipping mise self-update - you may be offline."
 
 log "Trusting mise config (see: https://mise.jdx.dev/cli/trust.html)."
 mise trust --yes || true
